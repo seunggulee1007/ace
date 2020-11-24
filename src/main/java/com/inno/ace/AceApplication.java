a10b76ac199eb5cec2d46023ec7926042aac5fc8
leeseunggu <leesg107@naver.com>
2020-11-25 05:27:59 +0900

    톰캣 의존성 추가

--- src/main/java/com/inno/ace/AceApplication.java
+++ src/main/java/com/inno/ace/AceApplication.java
@@ -2,12 +2,19 @@ package com.inno.ace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class AceApplication {
+public class AceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AceApplication.class, args);
